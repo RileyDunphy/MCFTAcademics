3c2053f9beb83613ee2b44ae027ed8c81cecd14f
Riley Dunphy <rileydunphy@live.ca>
2020-01-20 10:05:12 -0400

    Created a login page with authentication, right now just a dummy username and password

---
+++ MCFTAcademics/Pages/Login.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace MCFTAcademics
+{
+    public class LoginModel : PageModel
+    {
+        //A lot of this code came from this tutorial: 
+        //http://future-shock.net/blog/post/creating-a-simple-login-in-asp.net-core-2-using-authentication-and-authorization-not-identity
+        [BindProperty]
+        public LoginData loginData { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var isValid = (loginData.Username == "username" && loginData.Password == "password");
+                if (!isValid)
+                {
+                    ModelState.AddModelError("", "username or password is invalid");
+                    return Page();
+                }
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme, ClaimTypes.Name, ClaimTypes.Role);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, loginData.Username));
+                identity.AddClaim(new Claim(ClaimTypes.Name, loginData.Username));
+                var principal = new ClaimsPrincipal(identity);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties { IsPersistent = loginData.RememberMe });
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "username or password is blank");
+                return Page();
+            }
+        }
+
+        public class LoginData
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required, DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
+        }
+    }
+}
--- MCFTAcademics/Startup.cs
+++ MCFTAcademics/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace MCFTAcademics
 {
@@ -31,6 +27,12 @@ namespace MCFTAcademics
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(options => { options.LoginPath = "/Login"; });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
