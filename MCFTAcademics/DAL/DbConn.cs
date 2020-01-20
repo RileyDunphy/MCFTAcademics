1465cd595399c7615fb44c6c90abb2066a8642e0
Riley Dunphy <rileydunphy@live.ca>
2020-01-20 11:36:04 -0400

    Added the database connection string to the project, and the DbConn object, login method is checking the database to see if user exists

---
+++ MCFTAcademics/DAL/DbConn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCFTAcademics.DAL
+{
+    public class DbConn
+    {
+        public static SqlConnection GetConnection()
+        {
+            //get the connection string
+            String connString = "Data Source=ec2-18-207-246-135.compute-1.amazonaws.com;Persist Security Info=True;User ID=dev;Password=MarcuVision2020";
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
--- MCFTAcademics/Pages/Login.cshtml.cs
+++ MCFTAcademics/Pages/Login.cshtml.cs
@@ -5,6 +5,9 @@ using System.Security.Claims;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Data.SqlClient;
+using MCFTAcademics.DAL;
+using System;
 
 namespace MCFTAcademics
 {
@@ -19,7 +22,7 @@ namespace MCFTAcademics
         {
             if (ModelState.IsValid)
             {
-                var isValid = (loginData.Username == "username" && loginData.Password == "password");
+                var isValid = loginData.Login();
                 if (!isValid)
                 {
                     ModelState.AddModelError("", "username or password is invalid");
@@ -48,6 +51,39 @@ namespace MCFTAcademics
             public string Password { get; set; }
 
             public bool RememberMe { get; set; }
+
+            public bool Login()
+            {
+                bool loggedIn;
+                string sql = "select username, password from [mcftacademics].[dbo].[Users] where username = @username AND password = @password";
+                SqlConnection conn = DbConn.GetConnection();
+                SqlCommand selectCommand = new SqlCommand(sql, conn);
+                selectCommand.Parameters.AddWithValue("@username", this.Username);
+                selectCommand.Parameters.AddWithValue("@password", this.Password);
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = selectCommand.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        loggedIn= true;
+                    }
+                    else
+                    {
+                        loggedIn= false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                    loggedIn= false;
+                }
+                finally
+                {//close the connection either way
+                    conn.Close();
+                }
+                return loggedIn;
+            }
         }
     }
 }
