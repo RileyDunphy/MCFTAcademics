182763ba514c6b99af882e5f0fec94204a797b28
Calvin Buckley <calvin@cmpct.info>
2020-02-05 10:04:13 -0400

    Add a download transcript route for controller
    This is our first controller! Stuff to learn.
    
    There's also some room to expand here (wire it up to pages, select which PDF, access control, etc)

---
+++ MCFTAcademics/Controllers/TranscriptController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCFTAcademics.Controllers
+{
+    [ApiController]
+    // XXX: We have to explicitly state authorizations here until we add
+    // automatic authorization to startup, the current ones only apply to
+    // Razor. For now, just mark this authorize.
+    [Authorize]
+    public class TranscriptController : ControllerBase
+    {
+        [Route("DownloadTranscript")]
+        [HttpGet]
+        public async Task<IActionResult> DownloadTranscriptAsync()
+        {
+            var bytes = await System.IO.File.ReadAllBytesAsync("./Reports/someReport.pdf");
+            return File(bytes, "application/pdf", "Example PDF");
+        }
+    }
+}
