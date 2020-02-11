6741351aef500106c0b8a00d45dc025f46072a42
Calvin Buckley <calvin@cmpct.info>
2020-02-11 09:46:07 -0400

    Add getting grades, improve grade calculation
    Huge TODOs on schema we should resolve before proceeding.

--- MCFTAcademics/DAL/GradeDAL.cs
+++ MCFTAcademics/DAL/GradeDAL.cs
@@ -2,17 +2,71 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MCFTAcademics.DAL
 {
-    public class GradeDAL
+    public static class GradeDAL
     {
-        public static Grade GradeFromRow(IDataReader reader)
+        static Grade GradeFromRow(IDataReader reader)
         {
-            return new Grade(Convert.ToDecimal(reader["grade"]), Convert.ToDateTime(reader["given"]), Convert.ToBoolean(reader["lock"]), Convert.ToDecimal(reader["hoursAttended"]),Convert.ToBoolean(reader["isSupplemental"]),Course.GetCourseById(Convert.ToInt32(reader["courseId"])));
+            var locked = (bool)reader["lock"];
+            var supplemental = (bool)reader["isSupplemental"];
+            var given = (DateTime)reader["given"];
+            var hoursAttended = (decimal)reader["hoursAttended"];
+            var grade = (decimal)reader["grade"];
+            // XXX: Switch query to a join and use CourseDAL
+            var course = Course.GetCourseById((int)reader["courseId"]);
+            // XXX: Preserve the staff stuff too?
+            return new Grade(grade, given, locked, hoursAttended, supplemental, course);
         }
 
-    }
+        public static IEnumerable<Grade> GetAllGrades()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = DbConn.GetConnection();
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "mcftacademics.dbo.Get_All_Grades";
+                command.CommandType = CommandType.StoredProcedure;
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    yield return GradeFromRow(reader);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+
+        // XXX: This is probably very wrong. CourseStaff is halfway a
+        // table for m:n relations, but can also represent temps... which
+        // the schema can't allow for. As such, instructors can only be
+        // represented by User since it's the only other PK usable, so...
+        // (This should really be changed.)
+        public static IEnumerable<Grade> GetGradesForInstructor(User staff)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = DbConn.GetConnection();
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "mcftacademics.dbo.Get_Grades_ByStaff";
+                command.CommandType = CommandType.StoredProcedure;
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                    yield return GradeFromRow(reader);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
 }
