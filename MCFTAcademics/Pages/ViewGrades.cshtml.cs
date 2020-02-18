6fde542f64f4a1bfb6c5dcb620d8e22808098169
Calvin Buckley <calvin@cmpct.info>
2020-02-18 09:59:00 -0400

    Merge view/view all pages into one view page with a different handler
    Removes a lot of code from the equation.

--- MCFTAcademics/Pages/ViewGrades.cshtml.cs
+++ MCFTAcademics/Pages/ViewGrades.cshtml.cs
@@ -11,7 +11,17 @@ namespace MCFTAcademics
     {
         public void OnGet()
         {
+            ViewData["Title"] = "Instructor Grades";
+            var courses = BL.Course.GetCoursesByInstructor(User.IdAsInt());
+            ViewData["ViewGrades_Courses"] = courses;
+        }
 
+        /* XXX: What role does this belong to? */
+        public void OnGetAll()
+        {
+            ViewData["Title"] = "All Grades";
+            var courses = BL.Course.GetAllCourses();
+            ViewData["ViewGrades_Courses"] = courses;
         }
     }
 }
