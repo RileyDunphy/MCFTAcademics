b5002981a11ae25ade9c3ae4e654f18aa8b68659
Calvin Buckley <calvin@cmpct.info>
2020-02-25 09:09:53 -0400

    Historical grading algorithm support
    We just have one for now. Not very data driven, but works for now.

---
+++ MCFTAcademics/BL/GradingAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCFTAcademics.BL
+{
+    internal enum GradingAlgorithm
+    {
+        Default
+    }
+}
