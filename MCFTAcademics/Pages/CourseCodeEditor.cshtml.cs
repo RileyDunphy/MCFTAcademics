a3f55862e81aace044abab8eca557ba6935d6046
Riley Dunphy <rileydunphy@live.ca>
2020-03-19 11:30:34 -0300

    Added semester field, coursecodeeditor page functional

--- MCFTAcademics/Pages/CourseCodeEditor.cshtml.cs
+++ MCFTAcademics/Pages/CourseCodeEditor.cshtml.cs
@@ -13,7 +13,7 @@ namespace MCFTAcademics
         [BindProperty]
         public Course course { get; set; }
         public string alertMessage { get; set; }
-        public void OnGetEditor(int id)
+        public void OnGet(int id)
         {
             this.course = BL.Course.GetCourseById(id);
             ViewData["Title"] = course.Name;
@@ -28,14 +28,24 @@ namespace MCFTAcademics
                 for (int i = 0; i < Convert.ToInt32(Request.Form["count"]); i++)
                 {
                     //Still not sure if the semester changes per course code or if thats a course dependent thing
-                    CourseCode coursecode =  new CourseCode(Request.Form["txtCode+" + i], Convert.ToDateTime(Request.Form["txtStartDate+" + i]), Convert.ToDateTime(Request.Form["txtEndDate+" + i]), 1);
+                    CourseCode coursecode =  new CourseCode(Request.Form["txtCode+" + i], Convert.ToDateTime(Request.Form["txtStartDate+" + i]), Convert.ToDateTime(Request.Form["txtEndDate+" + i]), Convert.ToInt32(Request.Form["semester+"+i]));
 
                     coursecodes.Add(coursecode);
+                }
+                bool result = CourseCode.UpdateCourseCodes(coursecodes, Convert.ToInt32(Request.Form["courseId"]));
+                if (result)
+                {
+                    this.alertMessage = "Course Codes have been updated";
+                    return RedirectToPage("CourseCodeEditor", new { id = Convert.ToInt32(Request.Form["courseId"]) });
+                }
+                else
+                {
+                    throw new Exception();
                 }
             }
             catch (Exception ex)
             {
-                return FailWithMessage("There was an exception from the system updating the course;" +
+                return FailWithMessage("There was an exception from the system updating the course codes;" +
                     "report this to an administrator: " + ex.Message);
             }
         }
