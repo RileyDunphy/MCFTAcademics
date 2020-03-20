7c157e65f2f7893d9993edd6a43a72bf8fbdf2a4
ATOMIC-GOBLIN <44297593+ATOMIC-GOBLIN@users.noreply.github.com>
2020-03-20 10:10:18 -0300

    removed unnecessary code

--- MCFTAcademics/BL/Reports/Report.cs
+++ MCFTAcademics/BL/Reports/Report.cs
@@ -6,50 +6,18 @@ namespace MCFTAcademics.BL.Reports
 {
     public class Report
     {
-        //private List<Grade> grades= new List<Grade>();
+        
         private List<ReportColumn> columns;
-
+        public List<ReportColumn> Columns { get => columns; set => columns = value; }
         public Report(List<ReportColumn> columns)
         {
             this.Columns = columns;
         }
         public Report(string program, int semester) {
             this.columns = ReportDAL.SelectReportData(program, semester);
-            fillGrades();
-        }
-
-        //public List<Grade> Grades { get => grades; set => grades = value; }
-        public List<ReportColumn> Columns { get => columns; set => columns = value; }
-
-        public bool fillGrades()
-        {
-            try
-            {
-                //sort by student ID
+            //sort by student ID (keeping student details together)
                 columns.Sort();
-
-                //some fields are hard coded as they are not used in reports
-                //foreach (ReportColumn col in columns)
-                //{
-                //    int studentId = col.StudentId;
-                //    decimal gradeAssigned = col.Grade;
-                //    DateTime given = DateTime.Now;
-                //    bool locked = false;
-                //    decimal hoursAttended = -1;
-                //    bool supplemental = col.Supplemental;
-                //    Course subject = Course.GetCourseById(col.CourseId);
-
-                //    //Grade Grade = new Grade(studentId, gradeAssigned, given, locked, hoursAttended, supplemental, subject);
-
-                //    //grades.Add(Grade);
-                //}
-
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
         }
+
     }
 }
