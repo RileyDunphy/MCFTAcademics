3aca212788519ae5c5b2d0a44840f918b159a71e
Calvin Buckley <calvin@cmpct.info>
2020-04-06 09:45:16 -0300

    Why is this file missing in master, but not dev? Fixed now.

---
+++ MCFTAcademics/Controllers/TranscriptController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCFTAcademics.Controllers
+{
+    [ApiController]
+    // XXX: We have to explicitly state authorizations here until we add
+    // automatic authorization to startup, the current ones only apply to
+    // Razor. For now, just mark this authorize.
+    [Authorize]
+    public class TranscriptController : ControllerBase
+    {
+        [Route("DownloadTranscript")]
+        [HttpGet("DownloadTranscript/{reportName}", Name = "DownloadTranscript")]
+        public async Task<IActionResult> DownloadTranscriptAsync(string reportName)
+        {
+            var bytes = await System.IO.File.ReadAllBytesAsync("./Reports/" + reportName + ".pdf");
+            // If you give this a filename, it demands a download instead of displaying inline.
+            return File(bytes, "application/pdf");
+        }
+    }
+}
